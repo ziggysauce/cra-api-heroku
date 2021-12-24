63e47c2128e493561220dde2a549cc5d30349cb1
ziggysauce <dtn3423@gmail.com>
2021-12-24 00:20:30 -0500

    init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+const { SERVER_PORT = 3001 } = process.env;
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get('/api', async (req, res) => {
+  res.status(200).json({ data: [{num: 1}, {num: 2}, {num: 3}] });
+});
+
+app.listen(SERVER_PORT);
+
+console.log(`API server is listening on port:${SERVER_PORT}`);
