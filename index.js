1aa20fdb210f3242b2ef6bccc2ebe4449223776d
ziggysauce <dtn3423@gmail.com>
2021-12-24 00:29:20 -0500

    api route

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const { SERVER_PORT = 3001 } = process.env;
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
+app.get('/', async (req, res) => {
+  res.status(200).json({ data: 'Server is connected!' });
+});
+
 app.get('/api', async (req, res) => {
   res.status(200).json({ data: [{num: 1}, {num: 2}, {num: 3}] });
 });
