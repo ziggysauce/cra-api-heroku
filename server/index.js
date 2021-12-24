f0790fd98e4c14b499acedca3b2f7f3e7cc03bf2
ziggysauce <dtn3423@gmail.com>
2021-12-24 00:42:23 -0500

    env changes

--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@ const path = require('path');
 require('dotenv').config();
 
 const app = express();
-const { SERVER_PORT = 3001 } = process.env;
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/', async (req, res) => {
   res.status(200).json({ data: 'Server is connected!' });
@@ -15,6 +14,6 @@ app.get('/api', async (req, res) => {
   res.status(200).json({ data: [{num: 1}, {num: 2}, {num: 3}] });
 });
 
-app.listen(SERVER_PORT);
+app.listen(process.env.PORT || 3000);
 
-console.log(`API server is listening on port:${SERVER_PORT}`);
+console.log(`API server is listening on port:${process.env.PORT || 3000}`);
